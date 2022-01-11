e9bd0303ba6cd49e37c854e7060b617df4e9a195
PavanKumar-Gudiwada <91024463+PavanKumar-Gudiwada@users.noreply.github.com>
2022-01-11 12:43:43 +0530

    Adding modular files

---
+++ ColorCodes.h
@@ -0,0 +1,19 @@
+#ifndef COLORCODES_H
+#define COLORCODES_H
+
+extern const char* MajorColorNames[];
+    
+extern const char* MinorColorNames[];
+extern int numberOfMajorColors; 
+
+extern int numberOfMinorColors;
+
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
+
+#endif
---
+++ TestColorNumberPair.h
@@ -0,0 +1,10 @@
+#ifndef TESTCOLORNUMBERPAIR_H__
+#define TESTCOLORNUMBERPAIR_H__
+
+#include "ColorCodes.h" //MajorColor, MinorColor, ColorPair
+
+extern void ColorPairToString(const ColorPair* colorPair, char* buffer);
+extern void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+extern void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
+    
+#endif
